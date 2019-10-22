7c99a4cf84b1e11ddaf8aa1ec2ca32513f4d6686
Macky Fitz <pixelgamerx32@gmail.com>
2019-10-22 12:21:41 +0100

    Added javascript command files.
    This is for a command handler rewrite in the beta version of the bot.

---
+++ cora_modules/cora.cmds/play.js
@@ -0,0 +1,75 @@
+const {
+    Util
+} = require('discord.js')
+const ytdl = require('ytdl-core')
+// to be written using existing 'play' command with modifications
+module.exports = {
+    name: 'play',
+    description: "Plays a song in member's voice channel!",
+    async execute(message, serverQueue) {
+        const args = message.content.split(' ');
+    
+        const voiceChannel = message.member.voiceChannel;
+        if (!voiceChannel) {
+          console.log("Error! TargetChannel_voicechat.channelNotSpecified")
+          return message.channel.send('I need a voicechannel to play music!');} 
+        const permissions = voiceChannel.permissionsFor(message.client.user);
+        if (!permissions.has('CONNECT') || !permissions.has('SPEAK')) {
+          console.log("Error! InsufficientPerms_voicechat.channelPermsErr")
+          return message.channel.send("Insufficient permissions for users channel! Check bot and channel's permissions!");
+        }
+      
+        const songInfo = await YTDL.getInfo(args[1]);
+        const song = {
+          title: songInfo.title,
+          url: songInfo.video_url,
+        };
+      
+        if (!serverQueue) {
+          const queueContruct = {
+            textChannel: message.channel,
+            voiceChannel: voiceChannel,
+            connection: null,
+            songs: [],
+            volume: 5,
+            playing: true,
+          };
+          queue.set(message.guild.id, queueContruct);
+          queueContruct.songs.push(song);
+          try {
+            var connection = await voiceChannel.join();
+            queueContruct.connection = connection;
+            play(message.guild, queueContruct.songs[0]);
+          } catch (err) {
+            console.log(err);
+            queue.delete(message.guild.id);
+            return message.channel.send(err);
+          }
+        } else {
+          serverQueue.songs.push(song);
+          //console.log(serverQueue.songs);
+          return message.channel.send(`${song.title} has been added to the queue!`);
+        }
+    },
+
+    play(guild, song) {
+        const serverQueue = queue.get(guild.id);
+
+        if (!song) {
+            serverQueue.voiceChannel.leave();
+            queue.delete(guild.id);
+            return;
+        }
+
+        const dispatcher = serverQueue.connection.playStream(YTDL(song.url))
+            .on('end', () => {
+                console.log('Bot.MusicEnd'); //Debug to check when song ends.
+                serverQueue.songs.shift();
+                play(guild, serverQueue.songs[0]);
+            })
+            .on('error', error => {
+                console.error(error);
+            });
+        dispatcher.setVolumeLogarithmic(serverQueue.volume / 5);
+    }
+}
---
+++ cora_modules/cora.cmds/userinfo.js
@@ -0,0 +1,10 @@
+// to be rewritten using cora's own userinfo command.
+module.exports = {
+	name: 'userinfo',
+	description: 'Get information about a user.',
+	execute(message) {
+		const member = message.mentions.members.first();
+		const user = member.user;
+		message.channel.send(`Name: ${user.username}, ID: ${user.id}, Username: ${user.lastMessage.member.nickname}`);
+	},
+};
