f20ec4ce91cb9d827811487905d5901025fb029b
Macky Fitz <pixelgamerx32@gmail.com>
2019-10-22 13:44:27 +0100

    Added Owner commands
    Added owner only commands: Restart and Shutdown.

---
+++ cora_modules/cora.cmds/restart.js
@@ -0,0 +1,12 @@
+module.exports = {
+	name: 'restart',
+    description: 'Restarts the bot and reconnects it back to discord.',
+    execute(message){
+        console.log("CoraBot restarting...")
+        message.channel.send("Restarting, I will be right back. :wink:")
+        .then(bot.user.setStatus("dnd"))
+        .then(bot.user.setActivity("rebooting..."))
+        .then(_msg=>bot.destroy())
+        .then(()=>bot.login(token));
+    }
+};
---
+++ cora_modules/cora.cmds/shutdown.js
@@ -0,0 +1,11 @@
+module.exports = {
+	name: 'shutdown',
+    description: 'Disconnects the bot from discord and shuts down cleanly',
+    execute(message){
+        console.log("CoraBot deactivating...")
+        message.channel.send("Shutting down. Good night... <:sleepycat:635163563878514688>")
+        .then(bot.user.setStatus("dnd"))
+        .then(bot.user.setActivity("shutting down..."))
+        .then(_msg=>bot.destroy());
+    }
+};
