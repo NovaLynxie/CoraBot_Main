ad84c7f14a75ee1bf2f5bd43ec0589f53e03ffde
Macky Fitz <pixelgamerx32@gmail.com>
2019-10-22 21:28:21 +0100

    Update restart.js
    Removing the 'version' variable requirement and cleaning up code.

--- cora_modules/cora.cmds/restart.js
+++ cora_modules/cora.cmds/restart.js
@@ -1,7 +1,7 @@
 module.exports = {
 	name: 'restart',
     description: 'Restarts the bot and reconnects it back to discord.',
-    execute(message, bot, token){
+    execute(message, bot, version, token){
         console.log("CoraBot restarting...")
         message.channel.send("Restarting, I will be right back. :wink:")
         .then(bot.user.setStatus("dnd"))
