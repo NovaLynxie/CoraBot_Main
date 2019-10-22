f1274a201724ac599ab600a4c9dd71b1718c8367
Macky Fitz <pixelgamerx32@gmail.com>
2019-10-22 20:06:41 +0100

    Update userdata.js
    Found my 'glitch'!!!
    I forgot to add code to this command file and change its name/desc on copying. No longer conflicts with play.js anymore! XD

--- cora_modules/cora.cmds/userdata.js
+++ cora_modules/cora.cmds/userdata.js
@@ -1,7 +1,16 @@
 module.exports = {
-    name: 'play',
-    description: "Plays a song in member's voice channel!",
-    execute(message, bot, token){
-
+    name: 'userdata',
+    description: "Debug command to dump data via bot from discord.",
+    execute(message){
+        if (!args.length) {
+            return channel.send("Unknown User! \n```usage: >userinfo <args> [@mention, userID]```");
+        }
+        let userData = message.guild.member(message.mentions.users.first() || message.guild.members.get(args[0]))
+        console.log(userData) //debug code to dump info into bot console
+        let fileWrite = Flatted.stringify(userData, null, 2)
+        var filePath = './cora_modules/cora.debug/datadump_userdata.json'
+        fs.writeFileSync(filePath, fileWrite)
+        console.log("debug.datadump_userdata -> " + filePath)
+        channel.send("Data dump completed successfully! \nCheck my console " + message.author.toString())
     }
 };
