222fcc2f9f4086cb393011217e351a82ccd7a022
Macky Fitz <pixelgamerx32@gmail.com>
2019-10-23 12:38:55 +0100

    Create command_template.js
    Adding a command template so users will know how to correctly add their own commands.

---
+++ cora_modules/cora.data/command_template.js
@@ -0,0 +1,10 @@
+//Template for commands to be included as executable command
+//Command file must go into ./cora_modules/cora.cmds as <cmd>.js
+module.exports = {
+    name: 'example_command',
+    description: "Command description goes here.",
+    execute(message) {
+        //Command code to be executed goes here.
+        //Must use 'message' to send messages back to channel.
+    }
+};
