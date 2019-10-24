9369803e1c6aca7d5f1c1d01b40958fef7ebd611
Macky Fitz <pixelgamerx32@gmail.com>
2019-10-24 13:33:54 +0100

    Update userdata.js
    Fixes missing command dependencies and properly defines the missing dependencies in the file.

--- cora_modules/cora.cmds/userdata.js
+++ cora_modules/cora.cmds/userdata.js
@@ -1,17 +1,28 @@
+const Flatted = require("flatted");
+const fs = require('fs');
 module.exports = {
     name: 'userdata',
     description: "Debug command to dump data via bot from discord.",
     aliases: ['datadump'],
+    cooldown: 5,
+    guildOnly: true,
     execute(message){
+        const args = message.content.split(' ');
         if (!args.length) {
             return channel.send("Unknown User! \n```usage: >userinfo <args> [@mention, userID]```");
         }
-        let userData = message.guild.member(message.mentions.users.first() || message.guild.members.get(args[0]))
-        console.log(userData) //debug code to dump info into bot console
-        let fileWrite = Flatted.stringify(userData, null, 2)
-        var filePath = './cora_modules/cora.debug/datadump_userdata.json'
-        fs.writeFileSync(filePath, fileWrite)
-        console.log("debug.datadump_userdata -> " + filePath)
-        channel.send("Data dump completed successfully! \nCheck my console " + message.author.toString())
+        try {
+            let userData = message.guild.member(message.mentions.users.first() || message.guild.members.get(args[0]))
+            console.log(userData) //debug code to dump info into bot console
+            let fileWrite = Flatted.stringify(userData, null, 2)
+            var filePath = './cora_modules/cora.debug/datadump_userdata.json'
+            fs.writeFileSync(filePath, fileWrite)
+            console.log("debug.datadump_userdata -> " + filePath)
+            message.channel.send("Data dump completed successfully! \nCheck my console " + message.author.toString())
+        } catch (error) {
+            console.error(error)
+            message.reply("Parse Error! Check console.")
+        }
+        
     }
 };
