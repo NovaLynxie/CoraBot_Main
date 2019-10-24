93d59214bf4b0a79b4c9cb626b0e12ae3e1c92a5
Macky Fitz <pixelgamerx32@gmail.com>
2019-10-24 13:38:38 +0100

    Update index.js
    Removes no longer used definitions for module dependencies from index.js as they have been moved elsewhere.
    It is too difficult to parse them via the execute(<variable>) code line so was defined per command file where needed.

--- index.js
+++ index.js
@@ -1,7 +1,5 @@
 // Loads required modules into the code.
 const Discord = require("discord.js");
-const YTDL = require("ytdl-core");
-const Flatted = require("flatted");
 // Links code to other required parts.
 const fs = require('fs');
 // Read information from files (core bot)
@@ -12,10 +10,9 @@ const {
 } = require('./config.json');
 
 // Variables for DiscordBot
-//const bot = new Discord.Client();
 const bot = new Client(); //Custom discord client.js replaces Discord.Client()
 bot.commands = new Discord.Collection();
-const queue = new Map();
+const cooldowns = new Discord.Collection();
 
 // Command files handler to parse <cmd>.js files.
 const cmdsDir = './cora_modules/cora.cmds'
@@ -25,7 +22,7 @@ console.log("Fetching commands from cmdFiles and storing into table...")
 for (const file of cmdFiles) {
   const cmds = require(`./cora_modules/cora.cmds/${file}`)
   bot.commands.set(cmds.name, cmds)
-  console.log("Added "+file) //Debug console printout to confirm data.
+  //console.log("Added "+file) //Debug console printout to confirm data.
 }
 //console.log(bot.commands); //Debug console prompt to print all commands
 console.log("Commands table generated! Starting CoraBot...")
@@ -33,9 +30,8 @@ console.log("Commands table generated! Starting CoraBot...")
 // Bot.on Runtime
 bot.on('ready', () => {
   bot.user.setStatus('online')
-  bot.user.setActivity("with beta code o.O", {type:'PLAYING'});
-  console.log("CoraBot Beta ONLINE!")
-  console.log("WARNING! Running BETA core code! Some features may be incomplete!")
+  bot.user.setActivity("the guild", {type:'Watching'});
+  console.log("CoraBot is Online!")
 })
 bot.once('reconnecting', () => { 
   console.log('L.O.S! Attempting to reconnect...')
@@ -44,22 +40,52 @@ bot.once('disconnect', () => {
   console.log('Bot disconnected from Discord!')
 })
 
+// Process Error Handler - Catches any errors and attempt to prevent a bot crash.
+process.on('unhandledRejection', error => console.error('Uncaught Promise Rejection', error));
+
 // Bot Command Handler (Requires Command Files)
 bot.on('message', async message => {
+  // Parses args from command into args object.
   const args = message.content.slice(prefix.length).trim().split(/ +/g);
   const cmdName = args.shift().toLowerCase();
   const command = bot.commands.get(cmdName)
     || bot.commands.find(cmd => cmd.aliases && cmd.aliases.includes(cmdName));
-
+  
+  // Checks if command is set as guildOnly command.
+  if (command.guildOnly && message.channel.type !== 'text')
+    return message.reply('I can\'t execute that command inside DMs!');
+  
+  // Checks if message is from the bot and ignores it.
   if (message.author.bot) return;
   if (message.content.indexOf(prefix) !== 0) return;
 
+  // Checks for command cooldowns, if it has sets the cooldowns.
+  if (!cooldowns.has(command.name)) {
+    cooldowns.set(command.name, new Discord.Collection());
+  }
+  
+  const now = Date.now();
+  const timestamps = cooldowns.get(command.name);
+  const cooldownAmount = (command.cooldown || 3) * 1000;
+  
+  if (timestamps.has(message.author.id)) {
+    const expirationTime = timestamps.get(message.author.id) + cooldownAmount;
+  
+    if (now < expirationTime) {
+      const timeLeft = (expirationTime - now) / 1000;
+      return message.reply(`Please wait ${timeLeft.toFixed(1)} more second(s) before reusing the \`${command.name}\` command.`);
+    }
+  }
+
+  timestamps.set(message.author.id, now);
+  setTimeout(() => timestamps.delete(message.author.id), cooldownAmount);
+  
   try {
     command.execute(message, bot, token);
   } 
   catch (error) {
     console.error(error);
-    message.reply('Invalid command!')
+    message.reply('Handler Error!')
   }
 });
 
