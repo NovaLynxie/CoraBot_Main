16babb9af2d16881ae314ec5673b1631508527a9
Macky Fitz <pixelgamerx32@gmail.com>
2019-10-24 13:35:07 +0100

    Update userinfo.js
    Fixes dependency definitions and corrects errors in if statement.

--- cora_modules/cora.cmds/userinfo.js
+++ cora_modules/cora.cmds/userinfo.js
@@ -1,26 +1,33 @@
-const {
-	Discord
-} = require('discord.js');
+const Discord = require('discord.js');
 module.exports = {
 	name: 'userinfo',
 	description: 'Get information about a user.',
 	aliases: ['whois'],
+	guildOnly: true,
 	execute(message){
 		const args = message.content.split(' ');
-		if (!args.length) {
+		console.log(args);
+		if (!args[1]) {
+			message.channel.send("Missing args! \n```usage: >userinfo <args> [@mention, userID]```")
+		} else if (!args[2]){
+			let userData = message.guild.member(message.mentions.users.first() || message.guild.members.get(args[1]))
+			//console.log(userData) //debug code line to dump info to console
+			var embed = new Discord.RichEmbed()
+				.setDescription("__**Member Information**__")
+				.setColor(0x15f153)
+				.setThumbnail(userData.user.avatarURL)
+				.addField("Username", `${userData.user.username}#${userData.user.discriminator}`)
+				.addField("User ID", userData.id)
+				.addField("Online Status", `${userData.presence.status}`)
+				.addField("Created at", userData.user.createdAt)
+				.addField("Joined at", userData.joinedAt)
+			message.channel.send(embed)
+		} else if (args[2]==='-legacy'){
+			const member = message.mentions.members.first();
+			const user = member.user;
+			message.channel.send(`Name: ${user.username}, ID: ${user.id}, Username: ${user.lastMessage.member.nickname}`);
+		} else {
 			return message.channel.send("Unknown User! \n```usage: >userinfo <args> [@mention, userID]```");
 		}
-		let userData = message.guild.member(message.mentions.users.first() || message.guild.members.get(args[0]))
-		//console.log(userData) //debug code line to dump info to console
-		var embed = new Discord.RichEmbed()
-			.setDescription("__**Member Information**__")
-			.setColor(0x15f153)
-			.setThumbnail(userData.user.avatarURL)
-			.addField("Username", `${userData.user.username}#${userData.user.discriminator}`)
-			.addField("User ID", userData.id)
-			.addField("Online Status", `${userData.presence.status}`)
-			.addField("Created at", userData.user.createdAt)
-			.addField("Joined at", userData.joinedAt)
-		message.channel.send(embed)
 	},
 };
