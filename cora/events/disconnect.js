7a7600a3f379aac7712030bf38a735002998da41
Macky Fitz <pixelgamerx32@gmail.com>
2021-05-21 17:53:58 +0100

    Tweaked disconnect.js
    Added 'event' argument into function call here.

--- cora/events/disconnect.js
+++ cora/events/disconnect.js
@@ -3,7 +3,7 @@ const logger = require('../providers/WinstonPlugin');
 module.exports = {
   name: 'disconnect',
   execute(client) {
-    client.on('disconnect', () => {
+    client.on('disconnect', (event) => {
       logger.warn('Client websocket closed connection.')
     });
   },
