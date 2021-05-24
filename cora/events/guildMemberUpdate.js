c19a35a16a73020f0f6885958bac09252509a8e1
Macky Fitz <>
2021-05-24 09:21:41 +0000

    Fixing indentation in event files. Adjusted indentation in some event files.

--- cora/events/guildMemberUpdate.js
+++ cora/events/guildMemberUpdate.js
@@ -2,15 +2,16 @@ const botlogs = require('../modules/botLogger');
 const logger = require('../providers/WinstonPlugin');
 
 module.exports = {
-    name: 'guildMemberUpdate',
-    execute(oldMember, newMember) {
-        const removedRoles = oldMember.roles.cache.filter(role => !newMember.roles.cache.has(role.id));
-        if (removedRoles.size > 0) {
-            logger.info(`Role ${removedRoles.map(r=>r.name)} removed from ${oldMember.displayName}.`)
-        };
-        const addedRoles = newMember.roles.cache.filter(role=>!oldMember.roles.cache.has(role.id));
-        if (addedRoles.size > 0) {
-            logger.info(`Role ${addedRoles.map(r=>r.name)} added to ${oldMember.displayName}.`)
-        };
-    }
+  name: 'guildMemberUpdate',
+  execute(oldMember, newMember) {
+    const removedRoles = oldMember.roles.cache.filter(role => !newMember.roles.cache.has(role.id));
+    if (removedRoles.size > 0) {
+      logger.info(`Role ${removedRoles.map(r=>r.name)} removed from ${oldMember.displayName}.`)
+    };
+    const addedRoles = newMember.roles.cache.filter(role=>!oldMember.roles.cache.has(role.id));
+    if (addedRoles.size > 0) {
+      logger.info(`Role ${addedRoles.map(r=>r.name)} added to ${oldMember.displayName}.`)
+    };
+    botlogs(event, msgdata, client);
+  }
 }
