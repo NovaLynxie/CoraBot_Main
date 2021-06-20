e66675bea4fb60189fe16807f4e0678280b83513
Macky Fitz <pixelgamerx32@gmail.com>
2021-06-20 09:21:07 +0100

    Update chatBotHandler.js
    Removed unused variable.

--- cora/handlers/chatBotHandler.js
+++ cora/handlers/chatBotHandler.js
@@ -1,6 +1,6 @@
 const ChatBot = require('discord-chatbot');
 const chatbot = new ChatBot({name: "Cora", gender: "Female"});
-let chatBotHandlerStatus = 0;
+
 module.exports = (message, client) => {
   let chatBotSettings = client.settings.get('chatbot', { enableChatBot: false});
   let {enableChatBot, chatChannels} = chatBotSettings;
