e9615f557c37d048e8cb267b0c65c81461729f1a
Macky Fitz <pixelgamerx32@gmail.com>
2021-07-30 14:33:46 +0000

    Fixed chatBotHandler.js Made it so it now checks if chatChannels setting is a string first. Also moved execution into a try/catch statement to prevent it from crashing the bot.

--- cora/handlers/chatBotHandler.js
+++ cora/handlers/chatBotHandler.js
@@ -9,19 +9,34 @@ module.exports = (message, client) => {
     name: (botName) ? botName : 'Cora',
     gender: (botGender) ? botGender : 'Female'
   });
+
   logger.verbose('chatbot.trigger.event');
   logger.verbose(`message=${message}`); logger.verbose(`enableChatBot=${enableChatBot}`);
-  if (enableChatBot) {
-    logger.debug('chatbot responding...')
-    chatChannels.forEach(chatChannel => {
-      logger.debug(`searching for channel with ID ${chatChannel}`)
-      if (chatChannel === message.channel.id) {
-        chatBotModule.chat(message).then(res => {
-          logger.debug(`sending message (${res}) to channel ${message.channel.name}`)
-          message.channel.send(res);
-        })
-      }
-    })
-  }
-}
 
+  function chatBotResponder(channel) {
+    if (channel === message.channel.id) {
+      chatBotModule.chat(message).then(res => {
+        logger.debug(`sending message '${res}' to channel ${message.channel.name}`);
+        message.channel.send(res);
+      })
+    } else {
+      logger.debug('Channel IDs do not match! Silently ignoring message.');
+    }
+  };
+  try {
+    if (enableChatBot) {
+      logger.debug('chatbot responding...')
+      if (typeof chatChannels === 'string') {
+        chatBotResponder(chatChannels);
+      } else {
+        chatChannels.forEach(chatChannel => {
+          logger.debug(`searching for channel with ID ${chatChannel}`)
+          chatBotResponder(chatChannel);
+        });
+      };
+    };
+  } catch (err) {
+    logger.warn('ChatBot.handler encountered an error!');
+    logger.error(err.message); logger.debug(err.stack);
+  };
+};
