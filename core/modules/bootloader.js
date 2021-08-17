357aab9bc6852ad23a60cccc90b4ecd4b2de96d3
Macky Fitz <pixelgamerx32@gmail.com>
2021-08-17 08:37:21 +0000

    Fixed incorrect path for main configuration. Caused an unusual crash error.

--- core/modules/bootloader.js
+++ core/modules/bootloader.js
@@ -1,29 +1,31 @@
 const toml = require('toml');
 const fs = require('fs');
 
-let authCfgData, mainCfgData;
+let authConfig, mainConfig; var fileData;
 try {
-  authCfgData = toml.parse(fs.readFileSync('./settings/auth.toml', 'utf-8'));
+  fileData = fs.readFileSync('./settings/auth.toml', 'utf-8');
+  authConfig = toml.parse(fileData);
 } catch (err) {
   console.error('Failed to load auth.toml configuration!');
   console.error(err.message); console.debug(err.stack);
   console.warn('Falling back to environment variables.');
 }
 try {
-  mainCfgData = toml.parse(fs.readFileSync('./settings/main.toml', 'utf-8'));
+  fileData = fs.readFileSync('./settings/main.toml', 'utf-8');
+  mainConfig = toml.parse(fs.readFileSync('./settings/main.toml', 'utf-8'));
 } catch (err) {
   console.error('Failed to load main.toml configuration!');
   console.error(err.message); console.debug(err.stack);
   console.warn('Cannot proceed with bot boot up.')
 }
 
-var {discord} = authCfgData;
+var {discord} = authConfig;
 var {discordToken, ytApiKey} = discord;
 if (!discordToken || discordToken === 'NOT_SET') {
   discordToken = process.env.DISCORD_TOKEN;
 }
 
-const {general} = mainCfgData;
+const {general} = mainConfig;
 var {globalPrefix, ownerIDs} = general;
 
 module.exports.credentials = {discordToken, ytApiKey};
