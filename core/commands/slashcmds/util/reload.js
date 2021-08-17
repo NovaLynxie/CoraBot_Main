337512d0c01a4335c553974800e5096fd3656bb0
Macky Fitz <pixelgamerx32@gmail.com>
2021-08-17 20:01:05 +0100

    Update reload.js
    Now added a working reload command. I think?

--- core/commands/slashcmds/util/reload.js
+++ core/commands/slashcmds/util/reload.js
@@ -1,3 +1,6 @@
+const { InteractionResponseType } = require('discord-api-types');
+const { loadPrefixCmds, loadSlashCmds } = require('../../../handlers/cmdloader');
+const logger = require('../../../plugins/winstonplugin');
 module.exports = {
   data: {
     name: 'reload',
@@ -6,6 +9,17 @@ module.exports = {
     description: "Allows reload of all commands without restarting bot."
   },
 	async execute(interaction, client) {
-    let guild = interaction.guild;
+    logger.warn('Reloading commands! This may take a while');
+    logger.debug('Reloading prefix commands.');
+    await loadPrefixCmds(client);
+    logger.debug('Reloading slash commands.');
+    await loadSlashCmds(client);
+    logger.info('Finished reloading commands!')
+    interaction.reply(
+      {
+        content: 'Reloaded commands!', 
+        ephemeral: true
+      }
+    )
 	},
 };
