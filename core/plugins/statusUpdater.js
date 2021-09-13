4f7f95b73f3a8917009c262c58f43e5cc6500a4d
Macky Fitz <pixelgamerx32@gmail.com>
2021-09-13 09:00:38 +0000

    Use let instead of multiple var sets.

--- core/plugins/statusUpdater.js
+++ core/plugins/statusUpdater.js
@@ -5,18 +5,18 @@ module.exports = (client) => {
 	// Setup interval timers to update status and database.
 	const statusTimer = setInterval(async () => {
 		// status updater
-		logger.verbose('ran task update_status');
+		logger.verbose('ran task update_status'); let statusType;
 		const index = Math.floor(Math.random() * (activities.length - 1) + 1);
 		if (index >= 0 && index <= 1) {
-			var statusType = 'PLAYING';
+			statusType = 'PLAYING';
 		}
 		else
 		if (index >= 2 && index <= 3) {
-			var statusType = 'LISTENING';
+			statusType = 'LISTENING';
 		}
 		else
 		if (index >= 4 && index <= 5) {
-			var statusType = 'WATCHING';
+			statusType = 'WATCHING';
 		}
 		client.user.setActivity(activities[index], { type: statusType });
 		logger.verbose(`Updated status to activity ${index} of ${activities.length - 1}`);
