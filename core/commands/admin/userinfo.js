f7267c9c528220b745f6c8c26abfbbe7e8cbf01d
Macky Fitz <pixelgamerx32@gmail.com>
2021-09-16 10:22:04 +0000

    Add user info command.

---
+++ core/commands/admin/userinfo.js
@@ -0,0 +1,49 @@
+const logger = require('../../plugins/winstonLogger');
+const moment = require('moment');
+const { SlashCommandBuilder } = require('@discordjs/builders');
+const { MessageEmbed } = require('discord.js');
+const { stripIndents } = require('common-tags');
+
+module.exports = {
+  data: new SlashCommandBuilder()
+    .setName('user')
+    .setDescription('Gets user information and returns an embed.')
+    .addUserOption(option => 
+      option
+        .setName('target')
+        .setDescription('Select a user')
+        .setRequired(true)
+    ),
+  async execute (interaction, client) {
+    await interaction.deferReply({ ephemeral: true });
+    const data = interaction.options.getMember('user');
+    const user = interaction.user; const member = interaction.member;
+    const userInfoEmbed = new MessageEmbed()
+      .setTitle('User Information')
+      .setColor(0xE7A3F0)
+      .setDescription('Provides detailed information about any users in a guild.')
+      .addFields(
+        {
+          name: '> Member Information',
+          value: stripIndents`
+            Username: ${user.username}#${user.discriminator}
+            Nickname: ${member.nickname ? member.nickname : "N/A"}
+            Joined: ${moment.utc(member.joinedTimestamp).format('dddd, MMMM Do YYYY, HH:mm:ss Z')}
+            Roles: ${roles.length}
+            (${roles.length ? roles.join(', ') : "None"})
+          `
+        },
+        {
+          name: '> User Information',
+          value: stripIndents`
+            Bot Acc: ${user.bot}
+            Created: ${moment.utc(user.createdAt).format('dddd, MMMM Do YYYY, HH:mm:ss Z')}
+            Status: ${user.presence.status}
+            Game: ${user.presence.game ? user.presence.game.name : 'None'}
+          `
+        }
+      )
+      .setThumbnail(user.displayAvatarURL({ format: 'png' }))
+    return interaction.editReply({ embeds: [userInfoEmbed] });
+  }
+};
