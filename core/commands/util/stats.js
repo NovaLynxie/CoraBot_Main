0ce08079faa9a89c11221538401e0e733ce6dae3
Macky Fitz <pixelgamerx32@gmail.com>
2021-09-16 10:48:36 +0000

    Add/remove commands. Added stats command. Removed redundant help command. Moved info command.

---
+++ core/commands/util/stats.js
@@ -0,0 +1,44 @@
+const { MessageEmbed } = require('discord.js');
+const moment = require('moment');
+require('moment-duration-format');
+const { stripIndents } = require('common-tags');
+const { version } = require('../../../package.json');
+
+module.exports = {
+  data: {}
+  execute(interaction, client) {
+    var Servers = client.guilds.cache.size
+    var Channels = client.channels.cache.size
+    var Users = client.users.cache.filter(user => !user.bot).size
+    const statsEmbed = new MessageEmbed()
+      .setTitle("Bot Statistics")
+      .setColor(0xE7A3F0)
+      .addFields(
+        {
+          name: '> Total Uptime',
+          value: moment.duration(client.uptime)
+            .format('d[ days], h[ hours], m[ minutes ]s[ seconds]'),
+          inline: true
+        },
+        {
+          name: '> Memory Usage',
+          value: `${Math.round(process.memoryUsage().heapUsed / 1024 / 1024)}MB`
+        },
+        {
+          name: '> General Stats',
+          value: stripIndents`
+              Guilds: ${Servers} servers
+              Channels: ${Channels} channels
+              Users: ${Users} users
+              `
+        },
+        {
+          name: '> Version',
+          value: `v${version}`,
+          inline: true
+        }
+      )
+      .setThumbnail(client.user.displayAvatarURL({ formant: 'png' }))
+    return interaction.editReply({ embeds: [statsEmbed] });
+  }
+};
