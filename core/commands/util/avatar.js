55c8f17e1a15f3b7128288597a7d1e8d5e4d109f
Macky Fitz <pixelgamerx32@gmail.com>
2021-09-20 09:44:22 +0000

    Fix broken reply code.

--- core/commands/util/avatar.js
+++ core/commands/util/avatar.js
@@ -4,22 +4,21 @@ const { MessageEmbed } = require('discord.js');
 module.exports = {
 	data: new SlashCommandBuilder()
 		.setName('avatar')
-		.setDescription('Replies with \'Pong\' and response time.')
+		.setDescription('Fetches mentioned user\'s avatar and displays in an embed.')
     .addUserOption(option =>
 			option
 				.setName('target')
 				.setDescription('Enter any member mentionable or username.')
 				.setRequired(false),
 		),
-	async execute(interaction, client) {
-    await interaction.deferReply({ ephemeral: true });
+	execute(interaction, client) {
     const member = interaction.options.getMember('target');
     const avatarEmbed = new MessageEmbed()
       .setTitle('Stealing someones identity are we?')
       .setDescription(`Very well.. here is ${member.user.tag}'s current avatar.`)
       .setImage(member.user.displayAvatarURL({ dynamic: true }))
       .setFooter('Bot created and maintained by NovaLynxie.', client.user.displayAvatarURL({ format: 'png' }));
-		await interaction.reply(
+		return interaction.editReply(
 			{				
         embeds: [avatarEmbed],
 				ephemeral: true,
