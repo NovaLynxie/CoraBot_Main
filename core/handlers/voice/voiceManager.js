ddde3e44d2ba773dd743ef7cb0fbdbc6dba1d4d7
Macky Fitz <pixelgamerx32@gmail.com>
2021-09-20 13:14:46 +0000

    Clean up voice handler code.

--- core/handlers/voice/voiceManager.js
+++ core/handlers/voice/voiceManager.js
@@ -1,16 +1,6 @@
 const logger = require('../../plugins/winstonLogger');
 const {
-	NoSubscriberBehavior,
-	// StreamType,
-	// AudioPlayerStatus,
-	// VoiceConnectionDisconnectReason,
-	VoiceConnectionStatus,
-	createAudioPlayer,
-	createAudioResource,
-	// demuxProbe,
-	entersState,
-	joinVoiceChannel,
-	getVoiceConnection,
+	NoSubscriberBehavior, VoiceConnectionStatus, createAudioPlayer, createAudioResource, entersState, joinVoiceChannel, getVoiceConnection,
 } = require('@discordjs/voice');
 
 // Audio Player creation.
@@ -20,19 +10,20 @@ function newPlayer() {
 		maxMissedFrames: Math.round(5000 / 20),
 	});
 	return player;
-}
+};
 
 // Audio Source creation.
 function createSource(input) {
 	const audioSource = createAudioResource(input);
 	return audioSource;
-}
+};
 
 // Voice Connection functions.
 function checkVC(guild) {
 	const voiceConnection = getVoiceConnection(guild.id);
 	return voiceConnection;
-}
+};
+
 async function joinVC(channel) {
 	if (!channel) throw new Error('Missing channel data! Got null or undefined! Expected a channel object!');
 	logger.data(JSON.stringify(channel, null, 2));
@@ -41,19 +32,16 @@ async function joinVC(channel) {
 		guildId: channel.guild.id,
 		adapterCreator: channel.guild.voiceAdapterCreator,
 	});
-	// return voiceConnection;
 	try {
 		await entersState(voiceConnection, VoiceConnectionStatus.Ready, 30_000);
 		logger.debug('Connection successful! Ready to play audio.');
 		return voiceConnection;
-	}
-	catch (error) {
+	}	catch (error) {
 		logger.error('Connection error! Clearing connection!'); voiceConnection.destroy();
 		logger.error(error.message); logger.debug(error.stack);
 		logger.warn('Forced closed connection to save resources.');
-		// abort here if connection errored out.
 		throw new Error(error);
-	}
-}
+	};
+};
 
 module.exports = { checkVC, joinVC, createSource, newPlayer };
