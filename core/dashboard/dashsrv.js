74aca13bf3165a843dccf83762de22c7fc50b139
Macky Fitz <pixelgamerx32@gmail.com>
2021-10-07 11:56:37 +0000

    Start compacting / cleaning up code.

--- core/dashboard/dashsrv.js
+++ core/dashboard/dashsrv.js
@@ -1,19 +1,13 @@
 // Native Node Imports
 const url = require('url'), path = require('path'), fs = require('fs');
-
 // Extra Formatting
 const { stripIndents } = require('common-tags');
-
-
 // Fetch Bot version
 const { version } = require('../../package.json');
-
 // Winston Logger Plugin
 const logger = require('../plugins/winstonLogger');
-
 // For Discord Permission Handling
 const Discord = require('discord.js');
-
 // Express Session
 const express = require('express');
 const app = express();
@@ -40,7 +34,6 @@ module.exports = async (client, config) => {
 		if (!config.sessionSecret) logger.warn('Session Secret not set! May cause problems.');
 		logger.debug('Dashboard configuration OK. Continuing with startup.');
 	})();
-
 	// Initialise morgan logger for server side logging. (debug only)
 	if (config.debug) {
 		app.use(morgan('tiny', {
@@ -48,27 +41,18 @@ module.exports = async (client, config) => {
 				write: message => logger.debug(`dash => ${message}`),
 			},
 		}));
-	}
-	// Dashboard root directory from bot working directory.
+	};
 	const dashDir = path.resolve(`${process.cwd()}/core/dashboard`);
-	// Public and Views resource paths within Dashboard directory.
 	const viewsDir = path.resolve(`${dashDir}/views/`);
 	const publicDir = path.resolve(`${dashDir}/public/`);
 	app.use('/public', express.static(publicDir));
-	// Passport user handlers. Internal things for passport module.
-	// Do not touch these, just leave them be.
 	passport.serializeUser((user, done) => {
 		done(null, user);
 	});
 	passport.deserializeUser((obj, done) => {
 		done(null, obj);
 	});
-	// Defines Passport oauth2 data needed for dashboard authorization.
-	// Requires clientID, clientSecret and callbackURL.
-	// - clientID is the bot's unique client identification string.
-	// - clientSecret is a secret code for oauth2 handling.
-	// - callbackURL is the url to handle discord api callbacks.
-	passport.use(new DiscordStrategy({
+  passport.use(new DiscordStrategy({
 		clientID: config.clientID,
 		clientSecret: config.oauthSecret,
 		callbackURL: config.callbackURL,
@@ -78,29 +62,22 @@ module.exports = async (client, config) => {
 	(accessToken, refreshToken, profile, done) => {
 		process.nextTick(() => done(null, profile));
 	}));
-	// Session Data
-	// This is used for temporary storage of your visitor's session information. It contains secrets that should not be shared publicly.
-	app.use(session({
+  app.use(session({
 		// session storage location
 		store: new SQLiteStore({
 			db: 'sessions.db',
 			dir: './data/',
 		}),
-		// session secret - verification step
 		secret: process.env.SESSION_SECRET || config.sessionSecret,
 		// session cookie - remove after one week elapses
 		// cookie: { maxAge: 7 * 24 * 60 * 60 * 1000 }, //disabled temporarily
-		// session options
 		resave: false,
 		saveUninitialized: false,
 		unset: 'destroy',
 	}));
-
-	// Initializes passport and session.
 	app.use(passport.initialize());
 	app.use(passport.session());
-	// Initializes helmet with these options for all site pages.
-	app.use(helmet({
+  app.use(helmet({
 		contentSecurityPolicy: {
 			useDefaults: true,
 			directives: {
@@ -132,64 +109,39 @@ module.exports = async (client, config) => {
 				],
 				upgradeInsecureRequests: [],
 			},
-			// used for debugging purposes to determine if csp is working.
 			reportOnly: config.reportOnly || false,
 		},
 	}));
-
-	// The PUG templating engine allows for simpler setups and easy to read formatting in pages.
-	// It allows us separate header and footer components.
-	app.set('view engine', 'pug');
-
-	// body-parser reads incoming JSON or FORM data and simplifies their
-	// use in code.
-	app.use(express.json()); // to support JSON-encoded bodies
-	app.use(express.urlencoded({ // to support URL-encoded bodies
+  app.set('view engine', 'pug');
+	app.use(express.json());
+	app.use(express.urlencoded({
 		extended: true,
 	}));
-
-	// flash used for displaying alert messages
-	app.use(require('flash')());
-	/*
-  Authentication Checks. For each page where the user should be logged in, double-checks whether the login is valid and the session is still active.
-  */
+  app.use(require('flash')());
 	function checkAuth(req, res, next) {
 		if (req.isAuthenticated()) {
 			next();
-		}
-		else {
+		}	else {
 			logger.debug(`req.url='${req.url}'`);
 			req.session.backURL = req.url; res.status(401);
 			logger.debug(`req.session.backURL='${req.session.backURL}'`);
 			res.redirect('/login');
-		}
-	}
-	/*
-  Manage Server permission checks. This is a common check for all endpoints which need to confirm that the operator can edit the server region, roles, permissions, members, etc.
-  */
+		};
+	};
 	function isManaged(guild, dashuser) {
 		const member = guild.members.cache.get(dashuser.id);
 		const res = member.permissions.has('MANAGE_GUILD');
 		return res;
-	}
-	/*
-  Breadcrumb Fetcher. This uses the current page URL from ‘req’ to create breadcrumbs as an array of crumb objects, which is added to ‘req’.
-  Source: https://vidler.app/blog/javascript/nodejs/simple-and-dynamic-breadcrumbs-for-a-nodejs-express-application/
-  */
+	};
 	function get_breadcrumbs(url) {
 		let rtn = [{ name: 'HOME', url: '/' }],
 			acc = '', // accumulative url
 			arr = url.substring(1).split('/');
-
 		for (i = 0; i < arr.length; i++) {
 			acc = i != arr.length - 1 ? acc + '/' + arr[i] : null;
 			rtn[i + 1] = { name: arr[i].toUpperCase(), url: acc };
-		}
-		return rtn;
-	}
-	// This function simplifies the rendering of the page, since every page must be rendered
-	// with the passing of these 4 variables, and from a base path.
-	// Objectassign(object, newobject) simply merges 2 objects together, in case you didn't know!
+		}; return rtn;
+	};
 	const renderView = (res, req, template, data = {}) => {
 		logger.debug(`Preparing template ${template}`);
 		const baseData = {
@@ -208,22 +160,14 @@ module.exports = async (client, config) => {
 		logger.debug(`Rendering template ${template} with 'baseData' and 'data' parameters.`);
 		res.render(path.resolve(`${viewsDir}${path.sep}${template}`), Object.assign(baseData, data));
 	};
-
-	// Breadcrumb Handler Middleware
 	app.use(function(req, res, next) {
 		req.breadcrumbs = get_breadcrumbs(req.originalUrl);
 		next();
 	});
-
-	// Uptime Robot or Ping Service URL
 	app.get('/ping', (req, res, next) => {
 		res.send('DiscordBot/Dashboard Heartbeat Endpoint. Nothing to see here. :)');
 	});
-
 	// Dashboard Actions - All Interaction & Authentication actions.
-
-	// Login Endpoint
-	// Sends user to authenticate via discord oauth2.
 	app.get('/login', (req, res, next) => {
 		if (req.session.backURL) {
 			next();
@@ -240,48 +184,39 @@ module.exports = async (client, config) => {
 		next();
 	},
 	passport.authenticate('discord'));
-
 	// OAuth2 Callback Endpoint
-	// Once user returns, this is called to complete authorization.
 	app.get('/api/discord/callback', passport.authenticate('discord', { failureRedirect: '/autherror' }), (req, res) => {
 		logger.debug(`Checking req.user.id ${req.user.id} against owner IDs`);
 		logger.data(`client.options.owners => ${client.options.owners}`);
 		logger.data(`data type: ${typeof client.options.owners}`);
-		// Check if request user ID is an owner.
 		(client.options.owners.includes(req.user.id)) ? req.session.isAdmin = true : req.session.isAdmin = false;
 		if (req.session.isAdmin) {
 			logger.debug(`DiscordUser with ID:${req.user.id} logged in as 'ADMIN'.`);
-		}
-		else {
+		}	else {
 			logger.debug(`DiscordUser with ID:${req.user.id} logged in as 'USER'.`);
-		}
+		};
 		req.flash('success', 'Authenticated/Action Successful!');
 		if (req.session.backURL) {
 			const url = req.session.backURL;
 			req.session.backURL = null;
 			res.redirect(url);
-		}
-		else {
+		}	else {
 			res.redirect('/');
-		}
+		};
 	});
-
 	// Error Message - Show this if auth fails or action is interrupted.
 	app.get('/autherror', (req, res) => {
 		renderView(res, req, 'autherr.pug');
 	});
-
 	// Logout Endpoint - Destroys the session to log out the user.
 	app.get('/logout', function(req, res) {
 		req.session.destroy(() => {
 			req.logout();
 			req.flash('info', 'You have now been logged out of the dashboard.');
-			res.redirect('/'); // Inside a callback… bulletproof!
+			res.redirect('/');
 		});
 	});
-
 	// Dashboard Routes - Public and Authenticated site endpoints.
-
 	// Regular Information Pages (public pages)
 	app.get('/', (req, res) => {
 		renderView(res, req, 'index.pug');
@@ -323,16 +258,12 @@ module.exports = async (client, config) => {
 			},
 		});
 	});
-
 	// Authentication Locked Pages (Discord Oauth2)
-
 	// Normal Dashboard - Only shows user the guilds they are bound to.
 	app.get('/dashboard', checkAuth, (req, res) => {
-		// const perms = Discord.EvaluatedPermissions; //depreciated in discord.js v12+
 		const Permissions = Discord.Permissions;
 		renderView(res, req, 'dash.pug', { Permissions });
 	});
-
 	// Admin Dashboard - Shows all guilds the bot is connected to, including ones not joined by the user.
 	app.get('/admin', checkAuth, async (req, res) => {
 		const botSettings = await client.settings.get(client);
@@ -340,15 +271,11 @@ module.exports = async (client, config) => {
 		renderView(res, req, 'admin.pug', { botSettings });
 	});
 	app.get('/admin/reset_settings', checkAuth, async (req, res) => {
-		// Clear client settings and reset to default. (has no settings yet)
 		await client.settings.clear();
 		await client.settings.init();
-		// Fetch all guilds before running through them one by one.
 		const Guilds = client.guilds.cache.map(guild => guild);
 		Guilds.forEach(async guild => {
-			// Remove the guild's settings.
 			await client.settings.guild.delete(guild);
-			// Apply default settings using guild as reference for configuration.
 			await client.settings.guild.init(guild);
 			logger.debug(`${guild.name} settings reset!`);
 		});
@@ -372,14 +299,12 @@ module.exports = async (client, config) => {
 		req.flash('success', 'Saved preferences successfully!');
 		res.redirect('/admin');
 	});
-	// Simple redirect to the "Settings" page (aka "manage")
 	app.get('/dashboard/:guildID', checkAuth, (req, res) => {
 		res.redirect(`/dashboard/${req.params.guildID}/manage`);
 	});
-	// Settings page to change the guild configuration.
 	app.get('/dashboard/:guildID/manage', checkAuth, async (req, res) => {
 		const guild = await client.guilds.fetch(req.params.guildID);
-		const guildRoles = []; // prepare new array, then push one at a time.
+		const guildRoles = [];
 		guild.roles.cache.forEach(role => guildRoles.push(role));
 		if (!guild) return res.status(404);
 		if (!isManaged(guild, req.user) && !req.session.isAdmin) res.redirect('/');
@@ -388,8 +313,6 @@ module.exports = async (client, config) => {
 		logger.verbose(`guildSettings: ${JSON.stringify(guildSettings, null, 4)}`);
 		renderView(res, req, 'guild/manage.pug', { guild, guildRoles, guildSettings });
 	});
-
-	// This calls when settings are saved using POST requests to get parameters to save.
 	app.post('/dashboard/:guildID/manage', checkAuth, async (req, res) => {
 		logger.debug(`WebDash called POST action 'save_settings'!`);
 		logger.data(`req.body => ${JSON.stringify(req.body)}`);
@@ -491,7 +414,6 @@ module.exports = async (client, config) => {
 			req.flash('success', 'Saved settings successfully!');
 		}
 		catch (err) {
-			// Should it fail, catch and try to log the error from the bot/dashboard.
 			logger.warn('A setting failed to save correctly! Aborting settings change.');
 			logger.error(err.message); logger.debug(err.stack);
 			req.flash('danger', 'One or more settings failed to save! Please try again. If this error persists, ask an admin to check the logs.');
@@ -499,7 +421,6 @@ module.exports = async (client, config) => {
 		logger.debug('Redirecting to dashboard manage page.');
 		res.redirect('/dashboard/' + req.params.guildID + '/manage');
 	});
-
 	// Displays all members in the Discord guild being viewed.
 	app.get('/dashboard/:guildID/members', checkAuth, (req, res) => {
 		const guild = client.guilds.cache.get(req.params.guildID);
@@ -554,8 +475,7 @@ module.exports = async (client, config) => {
 			req.flash('warning', `Unable to kick ${member.user.tag}. Insufficient permissions or action was rejected by bot server.`);
 			logger.warn(`WebDash Operator BAN ${member.user.tag} aborted by DashService!`);
 			logger.warn(`Reason: The requested MemberID of ${member.user.tag}was the User's or Bot's unique ID.`);
-		}
-		else {
+		} else {
 			member.kick(`Kicked by WebDash Operator (ID :${req.user.id})`)
 				.then(req.flash('success', `${member.user.tag} has been removed from ${guild.name} successfully!`))
 				.catch(err => {
@@ -591,15 +511,12 @@ module.exports = async (client, config) => {
 		}
 		res.redirect('/dashboard');
 	});
-
 	// Fallback Middleware.
-
 	// 404 Not Found Handler (Only occurs if no error was thrown)
 	app.use(function(req, res, next) {
 		res.status(404);
 		renderView(res, req, 'errors/404.pug');
 	});
-
 	// Error Handling
 	app.use(function(err, req, res, next) {
 		logger.debug('Error occured in dashboard service!');
