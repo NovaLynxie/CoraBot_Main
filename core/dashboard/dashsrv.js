22bdcdebbbd158fd0bcd4e71cdf23663302e98e1
Macky Fitz <pixelgamerx32@gmail.com>
2021-10-25 10:48:52 +0000

    Cleaning up and updating dashboard code.

--- core/dashboard/dashsrv.js
+++ core/dashboard/dashsrv.js
@@ -236,17 +236,6 @@ module.exports = async (client, config) => {
       logger.verbose(`command: ${JSON.stringify(obj)}`);
       allcmds.push(obj);
     });
-    /*
-    const allcmds = `
-    ${appcmds.map(cmd => `
-    Name: ${cmd.name[0].toUpperCase() + cmd.name.substring(1)}
-    Desciption: ${cmd.description} 
-    Options: ${(cmd.options.length > 0) ? `${cmd.options.map(option => `
-      • ${option.name[0].toUpperCase() + option.name.substring(1)}
-        Desciption: ${option.description}
-        Required? ${(option.required) ? 'Yes' : 'No'}`).join('')}`
-        : 'None'}
-    `).join('\r')}`;*/
     renderView(res, req, "cmds.pug", {
       all_commands: allcmds
     });
