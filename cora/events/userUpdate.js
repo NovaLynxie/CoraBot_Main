454e6bb10f2ee581a267b0320bda973e755ef2b0
Macky Fitz <pixelgamerx32@gmail.com>
2021-10-26 12:11:07 +0000

    Fix missing 'client' parameter in userUpdate event.

--- cora/events/userUpdate.js
+++ cora/events/userUpdate.js
@@ -3,7 +3,7 @@ const logger = require('../providers/WinstonPlugin');
 
 module.exports = {
   name: 'userUpdate',
-  execute(oldUser, newUser) {
+  execute(oldUser, newUser, client) {
     let event = 'userUpdate', userdata = {oldUser, newUser};
     logger.data(`User's tag changed from ${oldUser.tag} to ${newUser.tag}`)
     botlogs(event, userdata, client);
