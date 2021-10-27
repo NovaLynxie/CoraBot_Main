57d81cc63aeb5b9edfea3578827cff89b47fbfb0
Macky Fitz <pixelgamerx32@gmail.com>
2021-10-27 13:00:37 +0000

    Add latency response to 'ping' command.

--- core/commands/util/ping.js
+++ core/commands/util/ping.js
@@ -1,13 +1,19 @@
 const { SlashCommandBuilder } = require('@discordjs/builders');
+const { stripIndents } = require('common-tags');
 
 module.exports = {
 	data: new SlashCommandBuilder()
 		.setName('ping')
 		.setDescription('Replies with \'Pong\' and response time.'),
 	async execute(interaction, client) {
-		await interaction.reply(
+    let latency = new Date().getTime() - interaction.createdTimestamp;
+    await interaction.reply(
 			{
-				content: `Pong! :heartpulse: ${client.ws.ping}ms`,
+				content: stripIndents`Ping Responder
+          \`\`\`
+          📡 ${client.ws.ping} ms
+          💬 ${latency} ms
+          \`\`\``,
 				ephemeral: true,
 			}
 		);
