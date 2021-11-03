01a482f6580a08ccea711f397fa8928e4e8c6605
Macky Fitz <pixelgamerx32@gmail.com>
2021-11-03 16:33:11 +0000

    Replace 'request' with 'node-fetch'.

--- core/plugins/urlParser.js
+++ core/plugins/urlParser.js
@@ -1,15 +1,20 @@
-const request = require('request');
+const fetch = require('node-fetch');
 const minify = require('url-minify');
 
 function validateURL (req) {
   return req.match(/(http|https):\/\/(\w+:{0,1}\w*@)?(\S+)(:[0-9]+)?(\/|\/([\w#!:.?+=&%@!\-\/]))?/);
 };
 
-function shortURL (url) {
+async function shortURL (url) {
   if (!validateURL(url)) return;
+  let res = await minify(url, { provider: 'isgd' });
+  return res.shortUrl;
 };
-function longURL (url) {
+async function longURL (url) {
   if (!validateURL(url)) return;
+  let res = await fetch(url);
+  logger.data(JSON.stringify(res));
+  return res;
 };
 
 module.exports = { shortURL, longURL };
