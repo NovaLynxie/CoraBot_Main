7fc53ab57a598118845a099704121a4ba5b02e50
Macky Fitz <pixelgamerx32@gmail.com>
2021-11-08 16:16:08 +0000

    Fix clear command?

--- core/commands/admin/clear.js
+++ core/commands/admin/clear.js
@@ -6,26 +6,37 @@ module.exports = {
   data: new SlashCommandBuilder()
     .setName('clear')
     .setDescription('Clear a select number of messages.')
-    .addIntegerOption(option => 
+    .addIntegerOption(option =>
       option
         .setName('limit')
         .setDescription('Number of messages to delete?')
         .setRequired(true)
     ),
   async execute(interaction, client) {
-		await interaction.deferReply({ ephemeral: true });
-    const limit = interaction.options.getInteger('limit');    
+    await interaction.deferReply({ ephemeral: true });
+    const limit = interaction.options.getInteger('limit');
     const executor = interaction.member; const guild = interaction.guild;
     const settings = await client.settings.guild.get(guild); const { roles } = settings;
-		if (executor.roles.cache.some(role => roles.staff.indexOf(role.id))) {
-	    guildLogger('clear', { executor }, client);
+    try {
+      await interaction.channel.bulkDelete(limit);
+    } catch (err) {
+      logger.debug(`Failed to delete messages in ${interaction.channel.name}!`);
+      await interaction.editReply({
+        content: 'Unable to delete messages! Possibly missing manage messages permissions?'
+        ephemeral: true
+      });
+      return;
+    }
+    
+    if (executor.roles.cache.some(role => roles.staff.indexOf(role.id))) {
+      guildLogger('clear', { executor }, client);
       interaction.editReply({
         content: `Cleared ${limit} messages successfully!`, ephemeral: true
       });
-		} else {
-			interaction.editReply({
-				content: 'You are not a staff member or are missing the required roles to use this command here!', ephemeral: true
-			});
-		};
+    } else {
+      interaction.editReply({
+        content: 'You are not a staff member or are missing the required roles to use this command here!', ephemeral: true
+      });
+    };
   }
 };
