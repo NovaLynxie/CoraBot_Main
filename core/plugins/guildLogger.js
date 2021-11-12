360b0c220b81934ec46fdae8439cc7cd32fb3b63
Macky Fitz <pixelgamerx32@gmail.com>
2021-11-12 15:31:04 +0000

    Some extra code work here and there.

--- core/plugins/guildLogger.js
+++ core/plugins/guildLogger.js
@@ -7,13 +7,14 @@ const guildBaseEmbed = new MessageEmbed()
   .setColor('#75e6c4');
 
 async function eventLog(event, guild, channel, params = {}, client) {
-  const { logChannels } = await client.settings.guild.get(guild);  
-  const msgdata = params ?.msgdata; const channel = params ?.channel;
-  const member = params ?.member;
+  const { logChannels } = await client.settings.guild.get(guild);
+  const { message, oldMessage, newMessage, member, oldMember, newMember } = params;
   const guildLogEmbed = new MessageEmbed(guildBaseEmbed)
     .setTitle('Event Log')
     .setFooter('Bot created and maintained by NovaLynxie.', client.user.displayAvatarURL({ format: 'png' }));
-  let memberDetails, messageDetails;
+  const hasChanged = (a, b) => a === b;
+  const shortenContents = (msg) => msg.length > 100 ? `${msg.content.substr(0, 97)}...` : msg.content;
+  let memberDetails, messageDetails, messageContents, oldMsgContents, newMsgContents;
   if (member) {
     memberDetails = {
       name: 'Member Details',
@@ -25,10 +26,50 @@ async function eventLog(event, guild, channel, params = {}, client) {
       `
     };
   };
+  if (oldMember || newMember) {
+    memberDetails = {
+      name: 'Member Updated',
+      value: stripIndents`
+        ${oldMember.user.tag} >> ${newMember.user.tag}
+        ${oldMember.nick} >> ${newMember.nick}
+        Avatar changed? ${(hasChanged(oldMember.avatar, newMember.avatar)) ? 'Yes' : 'No'}
+      `
+    };
+  };
+  if (message) {
+    messageDetails = {
+      name: 'Message Deleted',
+      value: stripIndents`
+        Created: ${time(message.createdAt)}
+        Edited: ${time(message.editedAt)}
+        Author: ${message.author}
+      `
+    };
+    messageContents = {
+      name: 'Message Content', value: message.content
+    };
+  };
+  if (oldMessage || newMessage) {
+    messageDetails = {
+      name: 'Message Updated',
+      value: stripIndents`
+        Created: ${time(message.createdAt)}
+        Edited: ${time(message.editedAt)}
+        Author: ${message.author}
+      `
+    };
+    oldMsgContents = {
+      name: 'Old Message', value: shortenContents(oldMessage.content)
+    };
+    newMsgContents = {
+      name: 'New Message', value: shortenContents(newMessage.content)
+    };
+  };
   switch (event) {
     case 'guildMemberAdd':
       guildLogEmbed
         .setDescription('User joined the server!')
+        .setThumbnail(member.displayAvatarURL())
         .addFields(memberDetails)
       break;
     case 'guildMemberRemove':
@@ -37,13 +78,17 @@ async function eventLog(event, guild, channel, params = {}, client) {
         .addFields(memberDetails)
       break;
     case 'guildMemberUpdate':
+      guildLogEmbed
+        .setDescription('User joined the server!')
+        .setThumbnail(newMember.displayAvatarURL())
+        .addFields(memberDetails)
       break;
     case 'messageDelete':
       break;
     case 'messageUpdate':
       break;
     default:
-      // ..
+    // ..
   }
   try {
     logger.debug('Sending moderation log to channel now...');
@@ -57,7 +102,7 @@ async function eventLog(event, guild, channel, params = {}, client) {
 }
 
 async function modLog(action, params = {}, client) {
-  const executor = params ?.executor, member = params ?.member;  
+  const executor = params ?.executor, member = params ?.member;
   const channel = params ?.channel ? params.channel : undefined;
   const guild = executor.guild, logdate = new Date();
   const reason = (params ?.reason) ? params.reason : 'No reason provided.';
@@ -134,7 +179,7 @@ async function modLog(action, params = {}, client) {
         )
       break;
     default:
-      // ..
+    // ..
   };
   try {
     logger.debug('Sending moderation log to channel now...');
