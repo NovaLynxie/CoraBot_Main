a8b826cac9eed68c8342042d618917066da10d06
Macky Fitz <pixelgamerx32@gmail.com>
2021-11-12 16:09:26 +0000

    Parse string directly as parameter.

--- core/events/messageDelete.js
+++ core/events/messageDelete.js
@@ -5,13 +5,12 @@ module.exports = {
 	name: 'messageDelete',
 	execute(message, client) {
     const guild = message.guild, channel = message.channel;
-    const event = 'messageDelete';
     if (message.author.id === null) {
       message.author.id = undefined;
     }
     if (message.author.id === client.user.id) {
       return logger.debug("Ignored message, message author is bot.");
     };
-    eventLog(event, guild, channel, { message }, client);
+    eventLog('messageDelete', guild, channel, { message }, client);
 	},
 };
