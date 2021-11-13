73b59793bca18d885d89068244ef30171be241d8
Macky Fitz <pixelgamerx32@gmail.com>
2021-11-13 14:53:14 +0000

    Add edited timestamp and content checks if no changes are made. Stops the bot from sending a message updated embed if there aren't any changes.

--- core/events/messageUpdate.js
+++ core/events/messageUpdate.js
@@ -4,6 +4,8 @@ const { eventLog } = require('../plugins/guildLogger');
 module.exports = {
   name: 'messageUpdate',
   execute(oldMessage, newMessage, client) {
+    if (oldMessage.editedTimestamp === newMessage.editedTimestamp) return;
+    if (oldMessage.content === newMessage.content) return;
     const guild = oldMessage.guild || newMessage.guild;
     const channel = oldMessage.channel || newMessage.channel;
     const member = oldMessage.member || newMessage.member;
