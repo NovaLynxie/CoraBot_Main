a02e2b97753791a615085e8d0e2e20013d4fe256
Macky Fitz <pixelgamerx32@gmail.com>
2021-11-16 11:21:23 +0000

    Always return the embed object.

--- core/plugins/botEmbeds.js
+++ core/plugins/botEmbeds.js
@@ -1,3 +1,21 @@
 const { MessageEmbed } = require('discord.js');
-
 const baseEmbed = new MessageEmbed().setColor('#75e6c4');
+
+function dynamicSystemEmbeds(state, params, client) {
+  const embed = new MessageEmbed(baseEmbed);
+  let details;
+  switch (state) {    
+    case 'success':
+      break;
+    case 'info':
+      break;
+    case 'warn':
+      break;
+    case 'error':
+      break;
+    case 'fatal':
+      break;
+    default;
+  };
+  return embed;
+};
