4aa1067ff02f967606fac7e0513effc2a05726e0
Macky Fitz <pixelgamerx32@gmail.com>
2021-11-30 16:13:08 +0000

    Revert to simple 'ping' command and remove subcommands from mc command.

--- core/commands/utility/mc.js
+++ core/commands/utility/mc.js
@@ -7,138 +7,74 @@ const { stripIndents } = require('common-tags');
 module.exports = {
 	data: new SlashCommandBuilder()
 		.setName('mc')
-		.setDescription('Minecraft Utility Command.')
-    .addSubcommand(subcommand =>
-      subcommand
-        .setName('ping')
-        .setDescription('Pings the server IP and waits for response.')
-        .addStringOption(option =>
-          option
-            .setName('type')
-            .setDescription('Java or Bedrock')
-            .setRequired(true)
-            .addChoice('Java','java')
-            .addChoice('Bedrock','bedrock')
-        )
-        .addStringOption(option =>
-          option
-            .setName('host')
-            .setDescription('Host IP')
-            .setRequired(true)
-        )
-        .addIntegerOption(option =>
-          option
-            .setName('port')
-            .setDescription('Port')
-            .setRequired(false)
-        )
+		.setDescription('Pings specified server IP and address.')
+    .addStringOption(option =>
+      option
+        .setName('type')
+        .setDescription('Java or Bedrock')
+        .setRequired(true)
+        .addChoice('Java','java')
+        .addChoice('Bedrock','bedrock')
     )
-    .addSubcommand(subcommand =>
-      subcommand
-        .setName('query')
-        .setDescription('Queries the server IP for information. (JAVA ONLY!)')
-        .addStringOption(option =>
-          option
-            .setName('request')
-            .setDescription('Basic or Full')
-            .setRequired(true)
-            .addChoice('Basic','basic')
-            .addChoice('Full','full')
-        )
-        .addStringOption(option =>
-          option
-            .setName('host')
-            .setDescription('Host IP')
-            .setRequired(true)
-        )
-        .addIntegerOption(option =>
-          option
-            .setName('port')
-            .setDescription('Port')
-            .setRequired(false)
-        )
+    .addStringOption(option =>
+      option
+        .setName('host')
+        .setDescription('Host IP')
+        .setRequired(true)
+    )
+    .addIntegerOption(option =>
+      option
+        .setName('port')
+        .setDescription('Port')
+        .setRequired(false)
     ),
 	async execute(interaction, client) {
     await interaction.deferReply();
     const defaultPorts = { 'java': 25565, 'bedrock': 19132 };
     const options = interaction.options;
-    const subcmd = options.getSubcommand();
-    const request = options.getString('request');
-    const type = (subcmd === 'query') ? 'java' : options.getString('type');
+    const type = options.getString('type');
     const host = options.getString('host');
     const port = options.getInteger('port');
     const cfg = { timeout: 5000, enableSRV: true };
-    let mcServerPingData, mcServerQueryData, mcOptions = {};
-    let mcEmbed = new MessageEmbed(), imgBuff, imgData;
+    let mcEmbed = new MessageEmbed(), mcServerData, mcOptions = {};
     if (host.toLowerCase() === 'localhost') return interaction.editReply({ content: "Please don't ping my mainframe. Any `localhost` request is disallowed and will simply be refused."});
-    try {      
+    try {
       switch (type) {
         case 'java':
-          mcOptions = { timeout: 10000, enableSRV: true };
-          if (subcmd === 'ping') {
-            mcServerPingData = await mcu.status(host, mcOptions, port);
-          };
-          if (subcmd === 'query') {
-            if (request === 'full') {
-              mcServerQueryData = await mcu.queryFull(host, mcOptions, port);
-            } else {
-              mcServerQueryData = await mcu.queryBasic(host, mcOptions, port);
-            };
-          };
+          mcOptions = { timeout: 5000, enableSRV: true };
+          mcServerData = await mcu.status(host, mcOptions, port);
           break;
         case 'bedrock':
-          mcOptions = { timeout: 10000, enableSRV: true };
-          mcServerPingData = await mcu.statusBedrock(host, mcOptions, port);
+          mcOptions = { enableSRV: true };
+          mcServerData = await mcu.statusBedrock(host, mcOptions, port);
           break;
+        default:
+          // ..
       };
+      const { description, onlinePlayers, maxPlayers, version, protocolVersion, favicon, roundTripLatency } = mcServerData;
+      const imgBuff = new Buffer.from(favicon.split(',')[1],'base64');
+      const imgData = new MessageAttachment(imgBuff, 'icon.png');
       mcEmbed 
         .setTitle('Minecraft Server')
         .setThumbnail('attachment://icon.png')
         .setColor('#836539')
-      if (mcServerPingData) {
-        const { description, onlinePlayers, maxPlayers, version, protocolVersion, favicon, roundTripLatency } = mcServerPingData;
-        imgBuff = new Buffer.from(favicon.split(',')[1],'base64');
-        imgData = new MessageAttachment(imgBuff, 'icon.png');        
-        mcServerResponse = {
-          name: 'Statistics',
-          value: stripIndents`
-            Players: ${onlinePlayers}/${maxPlayers}
-            Version: ${version}
-            Protocol: ${protocolVersion}
-            Latency: ${roundTripLatency}ms
-          `
-        };
-        mcEmbed
-          .setDescription(description.descriptionText)
-          .addFields(mcServerResponse)
-      };
-      if (mcServerQueryData) {  
-        const { favicon } = mcServerQueryData;
-        imgBuff = new Buffer.from(favicon.split(',')[1],'base64');
-        imgData = new MessageAttachment(imgBuff, 'icon.png');
-        if (request === 'full') {
-          mcServerResponse = {
-            name: 'Query Data [FULL]',
+        .setDescription(description.descriptionText)
+        .addFields(
+          {
+            name: 'Statistics',
             value: stripIndents`
-              
+              Players: ${onlinePlayers}/${maxPlayers}
+              Version: ${version}
+              Protocol: ${protocolVersion}
+              Latency: ${roundTripLatency}ms
             `
-          };
-        } else {
-          mcServerResponse = {
-            name: 'Query Data [BASIC]',
-            value: stripIndents`
-              
-            `
-          };
-        };
-        mcEmbed.addFields(mcServerResponse);
-      };
-      logger.debug(JSON.stringify(mcServerQueryData,null,2));
+          }
+        )
       await interaction.editReply({
         embeds: [mcEmbed], files: [imgData]
       });
     } catch (err) {
-      logger.debug(err.stack);
+      logger.error(err); logger.debug(err.stack);
       mcEmbed
         .setTitle('Minecraft Server')
         .setColor('#855038')
