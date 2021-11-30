227d49ad754ffcb25abeeadb27444ed329602191
Macky Fitz <pixelgamerx32@gmail.com>
2021-11-30 10:44:12 +0000

    Add missing GUILD_INVITES intent.

--- app.js
+++ app.js
@@ -21,6 +21,7 @@ const client = new Client({
 	intents: [
     Intents.FLAGS.DIRECT_MESSAGES,
 		Intents.FLAGS.GUILDS,
+    Intents.FLAGS.GUILD_INVITES,
 		Intents.FLAGS.GUILD_MESSAGES,
 		Intents.FLAGS.GUILD_MEMBERS,
 		Intents.FLAGS.GUILD_PRESENCES,
