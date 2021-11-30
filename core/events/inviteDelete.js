0f0a9a18687a5ae99bde4e0675d623280491732f
Macky Fitz <pixelgamerx32@gmail.com>
2021-11-30 10:45:35 +0000

    Fix wrong event name in inviteDelete.js

--- core/events/inviteDelete.js
+++ core/events/inviteDelete.js
@@ -3,7 +3,7 @@ const botEmbed = require('../utils/botEmbeds');
 const { eventLog } = require('../plugins/guildLogger');
 
 module.exports = {
-  name: 'inviteCreate',
+  name: 'inviteDelete',
   async execute(invite, client) {
     const guild = invite.guild;
     logger.debug(`Invite removed for ${guild.name}.`);
