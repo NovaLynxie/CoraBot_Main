d0028d7ccf4135e9ac16d2036c4be8213b145e76
Macky Fitz <pixelgamerx32@gmail.com>
2021-11-30 15:36:10 +0000

    Add mcutil query handlers.

--- core/commands/utility/mc.js
+++ core/commands/utility/mc.js
@@ -11,7 +11,40 @@ module.exports = {
     .addSubcommand(subcommand =>
       subcommand
         .setName('ping')
-        .setDescription('Pings specified server IP and address.')
+        .setDescription('Pings the server IP and waits for response.')
+        .addStringOption(option =>
+          option
+            .setName('type')
+            .setDescription('Java or Bedrock')
+            .setRequired(true)
+            .addChoice('Java','java')
+            .addChoice('Bedrock','bedrock')
+        )
+        .addStringOption(option =>
+          option
+            .setName('host')
+            .setDescription('Host IP')
+            .setRequired(true)
+        )
+        .addIntegerOption(option =>
+          option
+            .setName('port')
+            .setDescription('Port')
+            .setRequired(false)
+        )
+    )
+    .addSubcommand(subcommand =>
+      subcommand
+        .setName('query')
+        .setDescription('Queries the server IP for information. (JAVA ONLY!)')
+        .addStringOption(option =>
+          option
+            .setName('request')
+            .setDescription('Basic or Full')
+            .setRequired(true)
+            .addChoice('Basic','basic')
+            .addChoice('Full','full')
+        )
         .addStringOption(option =>
           option
             .setName('type')
@@ -38,24 +71,70 @@ module.exports = {
     const defaultPorts = { 'java': 25565, 'bedrock': 19132 };
     const options = interaction.options;
     const subcmd = options.getSubcommand();
+    const request = options.getString('request');
     const type = options.getString('type');
     const host = options.getString('host');
     const port = options.getInteger('port');
     const cfg = { timeout: 5000, enableSRV: true };
+    let mcServerResponse = {};
     let mcEmbed = new MessageEmbed(), mcServerData, mcOptions = {};
     if (host.toLowerCase() === 'localhost') return interaction.editReply({ content: "Please don't ping my mainframe. Any `localhost` request is disallowed and will simply be refused."});
     try {
       switch (type) {
         case 'java':
           mcOptions = { timeout: 5000, enableSRV: true };
-          mcServerData = await mcu.status(host, mcOptions, port);
+          if (subcmd === 'ping') {
+            mcServerData = await mcu.status(host, mcOptions, port);
+            mcServerResponse = {
+              name: 'Statistics',
+              value: stripIndents`
+                Players: ${onlinePlayers}/${maxPlayers}
+                Version: ${version}
+                Protocol: ${protocolVersion}
+                Latency: ${roundTripLatency}ms
+              `
+            };
+          };
+          if (subcmd === 'query') {
+            if (request === 'full') {
+              mcServerData = await mcu.queryFull(host, mcOptions, port);
+              mcServerResponse = {
+                name: 'Query [Full]',
+                value: stripIndents`
+                  Players: ${onlinePlayers}/${maxPlayers}
+                  Version: ${version}
+                  Protocol: ${protocolVersion}
+                  Latency: ${roundTripLatency}ms
+                `
+              };
+            } else {
+              mcServerData = await mcu.queryBasic(host, mcOptions, port);
+              mcServerResponse = {
+                name: 'Query [Basic]',
+                value: stripIndents`
+                  Players: ${onlinePlayers}/${maxPlayers}
+                  Version: ${version}
+                  Protocol: ${protocolVersion}
+                  Latency: ${roundTripLatency}ms
+                `
+              };
+            };
+              
+          };
           break;
         case 'bedrock':
           mcOptions = { enableSRV: true };
           mcServerData = await mcu.statusBedrock(host, mcOptions, port);
+          mcServerResponse = {
+            name: 'Statistics',
+            value: stripIndents`
+              Players: ${onlinePlayers}/${maxPlayers}
+              Version: ${version}
+              Protocol: ${protocolVersion}
+              Latency: ${roundTripLatency}ms
+            `
+          }
           break;
-        default:
-          // ..
       };
       const { description, onlinePlayers, maxPlayers, version, protocolVersion, favicon, roundTripLatency } = mcServerData;
       const imgBuff = new Buffer.from(favicon.split(',')[1],'base64');
@@ -65,17 +144,7 @@ module.exports = {
         .setThumbnail('attachment://icon.png')
         .setColor('#836539')
         .setDescription(description.descriptionText)
-        .addFields(
-          {
-            name: 'Statistics',
-            value: stripIndents`
-              Players: ${onlinePlayers}/${maxPlayers}
-              Version: ${version}
-              Protocol: ${protocolVersion}
-              Latency: ${roundTripLatency}ms
-            `
-          }
-        )
+        .addFields(mcServerResponse)
       await interaction.editReply({
         embeds: [mcEmbed], files: [imgData]
       });
