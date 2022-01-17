d4f937e38360c1c929be9a5aecab6abd3b969902
Macky Fitz <pixelgamerx32@gmail.com>
2022-01-17 16:32:38 +0000

    Change rateLimit warnings to debug instead.

--- core/events/rateLimit.js
+++ core/events/rateLimit.js
@@ -3,13 +3,13 @@ const logger = require('../utils/winstonLogger');
 module.exports = {
 	name: 'rateLimit',
 	execute(data) {
-		logger.fatal('Discord API Error! Rate Limit Reached!');
-    logger.fatal(`Timeout: ${data.timeout}`)
-    logger.fatal(`Limit: ${data.limit}`)
-    logger.fatal(`Method: ${data.method}`)
-    logger.fatal(`Path: ${data.path}`)
-    logger.fatal(`Route: ${data.route}`)
-    logger.fatal(`Global Route? ${(data.global) ? 'Yes' : 'No'}`)
+		logger.debug('Discord API Error! Rate Limit Reached!');
+    logger.debug(`Timeout: ${data.timeout}`)
+    logger.debug(`Limit: ${data.limit}`)
+    logger.debug(`Method: ${data.method}`)
+    logger.debug(`Path: ${data.path}`)
+    logger.debug(`Route: ${data.route}`)
+    logger.debug(`Global Route? ${(data.global) ? 'Yes' : 'No'}`)
     logger.data(JSON.stringify(data, null, 2));
 	},
 };
