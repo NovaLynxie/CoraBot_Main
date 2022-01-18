7433c5aad40732e3178b7f16749b3999116753e4
Macky Fitz <pixelgamerx32@gmail.com>
2022-01-18 16:14:12 +0000

    Remove unused/depreciated events. Turns out these events do not really do anything so removing these.

--- core/events/disconnect.js
+++
@@ -1,9 +0,0 @@
-const logger = require('../utils/winstonLogger');
-
-module.exports = {
-	name: 'disconnect',
-	execute() {
-		logger.debug('Disconnected! Will no longer attempt reconnect.');
-    logger.warn('Client disconnected from Discord.');    
-	},
-};
--- core/events/reconnect.js
+++
@@ -1,9 +0,0 @@
-const logger = require('../utils/winstonLogger');
-
-module.exports = {
-	name: 'reconnect',
-	execute() {
-		logger.debug('Reconnecting to discord. Please wait...');
-    logger.info('Attempting to reconnect to Discord.');
-	},
-};
