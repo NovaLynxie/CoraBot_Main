b7200fdf18b5e9952c613a283b4c9bae19ed8b6e
Macky Fitz <pixelgamerx32@gmail.com>
2022-01-25 13:00:52 +0000

    Stop debug log spam for each video here.

--- core/commands/music/music.js
+++ core/commands/music/music.js
@@ -190,7 +190,6 @@ module.exports = {
         for (let item of videos) {
           logger.verbose(JSON.stringify(item, null, 2));
           queue.push({ title: item.title, duration: item.durationInSec, url: item.url, thumbnail: item.thumbnails[item.thumbnails.length - 1].url, type: 'youtube' });
-          logger.debug(`Found ${item.title}`);
         };
       };
       logger.debug(`Parsed ${queue.length} songs! Adding them to music list.`);
