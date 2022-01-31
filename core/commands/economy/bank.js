f88f87552be2d7327986d52c868db1fe28c173ae
Macky Fitz <pixelgamerx32@gmail.com>
2022-01-31 09:56:10 +0000

    Start adding economy commands!

---
+++ core/commands/economy/bank.js
@@ -0,0 +1,9 @@
+const logger = require('../../utils/winstonLogger');
+const { SlashCommandBuilder } = require('@discordjs/builders');
+
+module.exports = {
+  data: new SlashCommandBuilder(),
+  async execute(interaction, client) {
+    //
+  }
+};
