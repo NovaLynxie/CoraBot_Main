e9729cd01894a1563f7f1fa2a6726147dde4302b
Macky Fitz <pixelgamerx32@gmail.com>
2022-01-31 12:22:34 +0000

    Add economy core plugin to handle most common functions. This will handle all of the economy common functions between all of the economy commands.

---
+++ core/plugins/economyCore.js
@@ -0,0 +1,21 @@
+const logger = require('../utils/winstonLogger');
+
+module.exports = (data, client) => {
+  function checkFunds(a, b) { return a > b };
+  function addFunds() {
+    // ..
+  };
+  function deductFunds() {
+    // ..
+  };
+  function transferFunds() {
+    // ..
+  };
+  function addItem() {
+    // ..
+  };
+  function removeItem() {
+    // ..
+  };
+  return data;
+};
